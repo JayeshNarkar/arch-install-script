b89134a91a27f556fd773b1cb739d9453d3dc39f
JayeshNarkar <jayesh.narkar18@gmail.com>
2024-07-10 12:39:09 +0530

    Init commit

---
+++ README.md
@@ -0,0 +1,111 @@
+# Arch Linux Installation Script
+
+## Overview
+
+This script is meant to provide a starting point for users who have used some linux distros before like ubuntu, but never got around to using distros like arch which requires you to configure things on your own.
+
+It can also be used to quickly setup an OS install.
+
+## Pre-requisites
+
+1. Creating a arch install boot loader.
+
+<img src="images/image-1.png" alt="image_showing_arch_loader" height="250">
+
+2. Freeing up space
+
+The minimum space required for the following types of installs is given below (feel free to free up more than that if you see fit):
+
+- **Minimal (no desktop environment)**: 11 GB
+- **GNOME or KDE Desktop Environment**: 41 GB
+- **Wayland Desktop Enviornment**: 41 GB
+
+Also do note that this does not include the swap partition size required (which is recommended to be the same size as your actual ram).
+
+(swap space is optional but beneficial, enhancing system performance by providing additional virtual memory when RAM is full)
+
+So if you have 8GB ram free up 41GB (for xfce/kde/wayland) + 8GB (for swap) = 49GB (or more)
+
+## Installation
+
+1. First clone the repository.
+
+```
+git clone https://github.com/JayeshNarkar/arch-install-script.git
+```
+
+2. Move the folder of the repo to your pendrive (which you have flashed arch installer iso on)
+
+## How to run
+
+1. Boot into the pendrive (where arch installer is loaded into)
+
+<img src="images/image.png" alt="image_showing_arch_loader" height="200">
+
+2. List storage devices and identify the pendrive
+
+<img src="images/lsblk_result.jpg" alt="image_showing_arch_loader" height="150">
+
+```
+lsblk
+```
+
+(in this case since my pendrive is 32 GBs, i can assume that the seperate sda partition of 28.7 GBs is it)
+
+(remember that the pendrive will be in a seperate group/tree from the other partitions and will be only 1 partition for a device if you have not modified anything)
+
+3. Mount the pendrive's partition to a folder
+
+<img src="images/mounting_pendrive.jpg" alt="image_showing_arch_loader" height="100">
+
+```
+mkdir /mnt/pendrive
+mount /dev/sda1 /mnt/pendrive
+ls /mnt/pendrive
+```
+
+4. Change the directory the pendrive's and make the script executable
+
+<img src="images/making_executable.jpg" alt="image_showing_arch_loader" height="80">
+
+```
+cd /mnt/pendrive/arch-install-script/
+chmod +x install.sh
+./install.sh
+```
+
+## Features
+
+- User-Friendly.
+
+- Interactive Execution: Step-by-step prompts for user control.
+
+- Informative: Details on each step's actions and methods.
+
+- Resumable: Picks up from the last completed step after exit.
+
+# Logging
+
+This script utilizes a log.txt file to keep track of how many steps have already been executed and how many are left. It is recommended that you do not change any contents of this file unnecessarily.
+
+But if something goes wrong incase of error in previous step.
+
+Feel free to delete the log file so that the script starts from first step again.
+
+## Reference
+
+How your partitions should look like after mounting and partioning:
+
+```
+lsblk
+```
+
+<img src="images/lsblk_after.png" alt="image_showing_arch_loader" height="200">
+
+How to check for network connectivity:
+
+```
+ping -c 1 google.com
+```
+
+<img src="images/ping_result.png" alt="image_showing_arch_loader" height="150">
---
+++ install.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+
+LOG_FILE="log.txt"
+
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
+starting_index=1
+
+if [ -f "$LOG_FILE" ]; then
+    starting_index=$(cat "$LOG_FILE")
+fi
+
+update_status() {
+    echo "$1" > $LOG_FILE
+}
+
+print_seperator(){
+     printf '%.0s-' {1..50}
+    echo -e "\n${BLUE}$1${NORMAL}${NC}\n"
+}
+
+print_slow_message(){        
+    message="$1"
+    color=${2^^} 
+    echo -e "${!color}"
+    for word in $message; do
+        echo -n "$word "  
+        sleep 0.5 
+    done
+    echo -e "${NORMAL}${NC}\n"
+}
+
+ask_network_connection() {
+    echo -e "Do you need to connect to WiFi, or are you already connected via Ethernet?"
+    echo -e "1) Connect to WiFi using iwctl"
+    echo -e "2) Already connected via Ethernet"
+    read -p "Please choose an option (1/2): " network_choice
+
+    case $network_choice in
+        1)
+            echo -e "${BOLD}Starting WiFi connection setup...${NORMAL}"
+            local wifi_device
+            local device_check_output
+            while true; do
+                read -p "Enter the device name you wish to connect with (e.g., wlan0): " wifi_device                
+                device_check_output=$(iwctl station "$wifi_device" show 2>&1)
+                if [[ $device_check_output == *"Device $wifi_device not found"* ]]; then
+                    echo -e "${RED}Device not found. Please enter a valid device name.${NC}"
+                else
+                    break
+                fi
+            done
+            echo -e "Scanning for WiFi networks..."
+            iwctl station "$wifi_device" scan
+            echo -e "Listing available networks..."
+            iwctl station "$wifi_device" get-networks
+            read -p "Enter the SSID of the WiFi network you wish to connect to: " wifi_ssid
+            read -sp "Enter the WiFi password: " wifi_password
+            echo
+            iwctl --passphrase "$wifi_password" station "$wifi_device" connect "$wifi_ssid"
+            ;;
+        2)
+            echo -e "${BOLD}Proceeding with Ethernet connection...\n${NORMAL}"
+            ;;
+        *)
+            echo -e "${RED}Invalid option selected. Please try again.${NC}"
+            ask_network_connection
+            ;;
+    esac
+}
+
+prompt_installation() {
+    local package_name=$1
+    local package_description=$2
+    local package_necessity=${3^^}    
+    local necessity_color=$NC 
+
+    if [[ $package_necessity == "RECOMMENDED" ]]; then
+        necessity_color=$RED
+    elif [[ $package_necessity == "OPTIONAL" ]]; then
+        necessity_color=$BLUE
+    fi
+    
+    echo -e "${BLUE}Package Name:${NC} ${package_name}"
+    
+    echo -e "\n${GREEN}$package_description${NC}"
+
+    echo -n -e "${BOLD}\nDo you wish to go through with its installation? ${necessity_color}(${package_necessity})${NC} (y/n): "
+    local answer
+    read answer
+    answer="${answer,,}"
+
+    while true; do 
+        case "$answer" in 
+            y | yes | "")
+                echo -e "${BOLD}Installing $package_name package...${NORMAL}"
+                sudo pacman -Sy "$package_name" --noconfirm
+                break ;;
+            n | no)
+                echo -e "${BOLD}Skipping $package_name installation...${NORMAL}\n"
+                break ;;
+            *)
+                read -rp "Please enter 'y' or 'n': " answer ;;
+        esac
+    done
+}
+
+prompt_exit() {
+  while true; do
+    echo -e "$1${NORMAL} (y/n) "
+    read answer
+    case $answer in
+      [Yy]* ) break;;
+      [Nn]* ) echo -e "\n$2"
+      echo -e "${BOLD}\nExiting script.${NORMAL}"; exit;;
+      * ) echo "Please answer y or n.";;
+    esac
+  done
+}
+
+show_steps_for_making_partitions(){
+    echo -e "\nThis next step requires some human intervention since identifying and creating partitions through a script is a tough task. But here are some steps to guide you through it:"
+    sleep 2
+
+    echo -e "\nStep 1. Identify the storage device you want to make the partitions on."
+    sleep 2
+
+    echo -e "\nStep 2. When prompted enter its name (nvme0n1,sda)"
+    sleep 2
+    
+    echo -e "\nStep 3. in the menu displayed there will be a section called free space and it will be in green. If you dont see it that means you either forgot to do what was instructed in the previous step. (to free up space) Or you just selected the wrong storage device"
+
+    sleep 2
+
+    echo -e "\nStep 4. Create 2-3 partitions. for boot and root (and swap (optional but highly recommended))"
+
+    sleep 2
+
+    echo -e "\nHere are some info on the partitions and the sizes you should give them:"
+    sleep 2
+
+    echo -e "\n1. Efi/boot: 500-800M (mb is M in that menu)"
+    sleep 2
+    
+    echo -e "\n2. Swap: 8G or 16G or however much your ram is(gb is G in that menu)"    
+    sleep 2
+
+    echo -e "\n3. Root: Rest of the remaining free space"    
+    sleep 2
+
+    echo -e "A video showing how to create these can also be found in the github repo of this script."    
+    sleep 2
+
+    echo -e "\nUse the arrow keys <- and -> to move from one option to another"
+    sleep 2
+
+    echo -e "\nStep 6. Go to the write button and press enter. Which should exit that menu and script will start running again!"
+    sleep 2
+}
+
+function1(){
+    print_slow_message "Welcome to Jayesh's Arch Linux install script!" "green"
+}
+
+function2(){
+    print_seperator "Network Connection Setup"
+    ask_network_connection
+}
+
+function3(){
+    prompt_installation "archlinux-keyring" "The archlinux-keyring package is fundamental to the security model of Arch Linux, ensuring that you can trust the packages you install from the official repositories." "recommended"
+}
+
+function4(){
+    print_seperator "Storage Allocation"
+    prompt_exit "Have you free'd up space on one of the storage devices connected to the system?" "Go ahead and do so in your windows OS (if you are setting up a dual boot) or use the cfdisk command provided by the arch installer in the current terminal that you are in.\nAlso this arch install script will start from here again instead of starting from the start so....you're welcome!"
+}
+
+function5(){
+    local answer
+    echo -e "\nDo you wish to see the steps to make the partitions?(y/n) "
+    read answer
+     if [[ $answer == "y" || $answer == "Y" ]]; then
+        show_steps_for_making_partitions
+    fi
+
+    echo -e "\nHave you already created the partitions?(y/n) "
+    read answer
+    if [[ $answer == "n" || $answer == "N" ]]; then
+        lsblk
+        sleep 1
+        echo -e "\n\nWhats the name of the storage device where you wish to install arch?(nvme0n1, sda) "
+        read storage_device_for_partitions
+        cfdisk /dev/${storage_device_for_partitions}
+    fi
+
+    echo -e "\nDid you create the 3 partitions?(y/n) "
+    read answer
+     if [[ $answer == "n" || $answer == "N" ]]; then
+        echo -e "Exiting script. Restart the script by typing ./install.sh. It should restart from the same step as you left"
+    else
+        echo -e "\Continuing..."
+     fi
+
+     echo -e "Enter the root partition (sda2, nvme0n1p3, or something) "
+     read root_partition
+     mkfs.ext4 /dev/${root_partition}
+     mount /dev/${root_partition} /mnt
+
+     echo -e "Enter the efi partition name (sda1, nvme0n1p2, or something): "
+     read efi_partition
+     mkdir /mnt/boot
+     mkfs.fat -F32 /dev/${efi_partition}
+     mount /dev/${efi_partition} /mnt/boot
+
+     echo -e "Do you have a swap partition?(y/n) "
+     read answer
+     if [[ $answer == "y" || $answer == "Y" ]]; then
+        echo -e "Enter the swap partition (sda2, nvme0n1p3, or something) "
+        read swap_partition
+        mkswap /dev/${swap_partition}
+        swapon /dev/${swap_partition}
+     fi
+}
+
+# cargo mpv bluez bluez-utils
+
+function6(){
+    print_seperator "Pac Strapping"
+
+    prompt_exit "\nBefore proceeding, this is a final time asking if you are sure you made all 3 partitions properly with proper types and are connected to network.\nDo you wish to check once more by exiting the script?(y/n) " "Images of how your lsblk after partioning should be present in the repo. And to check your network connectivity use the command ping -c 1 google.com"
+
+    echo -e "Do you have intel or amd based cpu?(intel/amd) "
+    read cpu_type
+
+    if [[ $cpu_type != "intel" && $cpu_type != "amd" ]]; then
+        echo "Invalid CPU type. Exiting script."
+        exit 1
+    fi
+
+    pacstrap -i /mnt base base-devel linux linux-headers linux-firmware ${cpu_type}-ucode sudo git nano vim neofetch htop networkmanager cmake make gcc --noconfirm
+
+    genfstab -U /mnt >> /mnt/etc/fstab
+
+    prompt_exit "The arch minimal package installation is done and now we will enter the arch environment using chroot. This script will only assist you so far, so clone part 2 of this script from the repo again after entering the root environment. Command for cloning: \"git clone https://github.com/JayeshNarkar/arch-install-script.git\". Wish to exit the script to perform some checks before entering? Or should we enter? (\"y\" to enter/ \"n\" to exit script) "
+
+    arch-chroot /mnt
+}
+
+for index in `seq $starting_index 6`; do
+    update_status $index
+    function${index} 
+done 
